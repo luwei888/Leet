2071a48f1ab2ba69c6b7d3b513dd1c9f96c0f624
luwei.jin@outlook.com <luwei.jin@outlook.com>
2020-06-28 19:35:35 +0800

    最佳股票交易时间

---
+++ LeetCode/3.101-150/TradeBestTime_121.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+
+namespace LeetCode._3._101_150
+{
+    /// <summary>
+    /// 买卖股票的最佳时机
+    /// </summary>
+    class TradeBestTime_121
+    {
+
+        public int[] Arrays { get; set; }
+
+        public TradeBestTime_121()
+        {
+            Arrays = new int[10000];
+            Random random = new Random();
+            for (int i = 0; i < 10000; i++)
+            {
+                Arrays[i] = random.Next(1, 500);
+            }
+        }
+
+        public int MyRun()
+        {
+            int temp = 0;
+            for (int i = 0; i < Arrays.Length; i++)
+            {
+                for (int j = i + 1; j < Arrays.Length; j++)
+                {
+                    var test = Arrays[j] - Arrays[i];
+                    if (test > temp)
+                    {
+                        temp = test;
+                    }
+                }
+            }
+            return temp;
+        }
+
+        public int BestRun()
+        {
+            int minPrice = int.MaxValue;
+            int maxPrice = 0;
+            for (int i = 0; i < Arrays.Length; i++)
+            {
+                if (Arrays[i] < minPrice)
+                {
+                    minPrice = Arrays[i];
+                }
+                else if (Arrays[i] - minPrice > maxPrice)
+                {
+                    maxPrice = Arrays[i] - minPrice;
+                }
+            }
+            return maxPrice;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LeetCode._3._101_150;
+using System;
 
 namespace LeetCode
 {
@@ -6,7 +7,19 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var tradeBestTime_121 = new TradeBestTime_121();
+
+            Console.WriteLine(DateTimeOffset.Now.Second + "+" + DateTimeOffset.Now.Millisecond);
+
+            Console.WriteLine(tradeBestTime_121.MyRun());
+
+            Console.WriteLine(DateTimeOffset.Now.Second + "+" + DateTimeOffset.Now.Millisecond);
+
+            Console.WriteLine(DateTimeOffset.Now.Second + "+" + DateTimeOffset.Now.Millisecond);
+
+            Console.WriteLine(tradeBestTime_121.BestRun());
+
+            Console.WriteLine(DateTimeOffset.Now.Second + "+" + DateTimeOffset.Now.Millisecond);
         }
     }
 }
